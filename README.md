87aa950ed41f9f628e37f42cb0756a0283ed0347
ledleledle <ledle0408@gmail.com>
2021-08-22 10:33:47 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Kong API Gateway
+
+### Access Konga
+```
+<your_ip>:1337
+Then create your own user
+```
+
+### Access Kong
+```
+# Kong Admin
+<your_ip>:8000
+# Kong Proxy Listener
+<your_ip>:8001
+```
+
+### Example App
+```
+<your_ip>:3000
+/slash
+/hello
+/lebah
+```
+
+Wait 1-2 minutes, until **Konga** ready to use!
---
+++ project/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const server = express()
+const port = 3000
+ 
+server.get('/one', (req, res) => {
+  console.log(req.headers)
+  res.status(200).send('API v1') 
+})
+
+server.get('/two', (req, res) => {
+  console.log(req.headers)
+  res.status(200).send('API v2')
+})
+
+server.get('/three', (req, res) => {
+  console.log(req.headers)
+  res.status(200).send('{"version": "API:3.0"}')
+})
+ 
+server.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`)
+})
